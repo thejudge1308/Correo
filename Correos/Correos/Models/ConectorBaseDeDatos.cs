2035243a0228711c53c6f6de525d6ceba4a42123
Patricio Quezada <the.judge16@gmail.com>
2018-08-07 12:55:11 -0400

    Implementacion de posgrest

---
+++ Correos/Correos/Models/ConectorBaseDeDatos.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Correos.Models
+{
+    public class ConectorBaseDeDatos
+    {
+        public static string path = "Server=127.0.0.1;User Id=postgres;" +
+                                   "Password=postgres;Database=correo;";
+        NpgsqlConnection conn;
+
+        public ConectorBaseDeDatos() {
+            this.conn = new NpgsqlConnection(path);
+            conn.Open();
+        }
+
+        public NpgsqlDataReader RealizarConsulta(string query) {
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public void CerrarConexion() {
+            this.conn.Close();
+        }
+    }
+}
