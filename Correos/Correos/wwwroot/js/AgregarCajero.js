b379e1dc29a947879ffcf5c26e1baccdb0cf112f
Patricio Quezada <the.judge16@gmail.com>
2018-08-08 13:44:25 -0400

    aGREAGR

--- Correos/Correos/wwwroot/js/AgregarCajero.js
+++ Correos/Correos/wwwroot/js/AgregarCajero.js
@@ -1,6 +1,18 @@
 ﻿$(document).ready(function () {
 
     console.log("asdasdasd");
+   
+    $("#Guardar").on('click', function () {
+        var rut = $("#Rut").val();
+        var nombre = $("#Nombre").val();
+        var sexo = $("#Sexo").val();
+        var domicilio = $("#Domicilio").val();
+        var fecha = $("#Fecha").val();
+        var fono = $("#Fono").val();
+        var opcion = $("#sele option:selected").text();
+
+        console.log(opcion + " : "+rut);
+    });
 
 
 
